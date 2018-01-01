4a59ab77ead98e2a6c64ad55810cdc77fc29e7e9
byverdu <byverdu@gmail.com>
2018-01-01 23:42:16 +0000

    added tests for hidden config

---
+++ test/config.test.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import {
+  describe, it, expect
+} from './testSetup';
+
+describe( 'initial config', () => {
+  it( 'Username should be a "env"', () => {
+    expect( process.env.CRYPTO_USERNAME ).not.eq( undefined );
+  });
+  it( 'Password should be a "env"', () => {
+    expect( process.env.CRYPTO_PASSWORD ).not.eq( undefined );
+  });
+  it( 'portfolio.json shouldn\'t be in source control', () => {
+    const fileContent = fs.readFileSync( './.gitignore', 'utf8' );
+    expect( fileContent ).to.include( 'app/data/portfolio.json' );
+  });
+});
---
+++ test/testSetup.js
@@ -0,0 +1,7 @@
+/* global describe, it */
+import { expect } from 'chai';
+import request from 'supertest';
+
+export {
+  expect, describe, it, request
+};
