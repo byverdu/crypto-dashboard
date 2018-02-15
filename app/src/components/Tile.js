e351463b1fb590aab772be37dc0f318186a5b317
byverdu <byverdu@gmail.com>
2018-02-15 01:25:05 +0000

    refactored tile component to call crypto API

--- app/src/components/Tile.js
+++ app/src/components/Tile.js
@@ -1,38 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Card, CardHeader, CardFooter, CardBody,
   CardTitle, ListGroup, ListGroupItem
 } from 'reactstrap';
-import { calculateTradingValue, fiatConverter } from '../../utils';
+import axios from 'axios';
+import { calculateTradingValue, getFiatSign, getFiatCodeLetter } from '../../utils';
 
-const Tile = ({
-  dateCrypto, nameCrypto, fiatCrypto, priceCrypto, amountCrypto
-}) => {
-  const fiatString = fiatConverter( fiatCrypto );
-  const tradeValue = calculateTradingValue( amountCrypto, priceCrypto );
-  return (
-    <Card>
-      <CardHeader tag="h3">
-        { nameCrypto }
-      </CardHeader>
-      <CardBody>
-        <CardTitle>
-          Position Details
-        </CardTitle>
-        <ListGroup>
-          <ListGroupItem color="secondary">
-            Trade date: {dateCrypto}
-          </ListGroupItem>
-          <ListGroupItem color="success">
-            Bought {amountCrypto} {nameCrypto} @ {priceCrypto} = {fiatString} {tradeValue}
-          </ListGroupItem>
-        </ListGroup>
-      </CardBody>
-      <CardFooter className="text-muted">
-        Trading @ API resp
-      </CardFooter>
-    </Card>
-  );
-};
+export default class Tile extends Component {
+  constructor( props ) {
+    super( props );
+    this.state = {
+      actualPrice: 0,
+      setIntervalId: 0
+    };
+  }
 
-export default Tile;
+  componentDidMount() {
+    this.setIntervalCallback();
+    const setIntervalId = setInterval( this.setIntervalCallback.bind( this ), 10000 );
+
+    this.setState({
+      setIntervalId
+    });
+  }
+
+  componentWillUnmount() {
+    clearInterval( this.state.setIntervalId );
+  }
+
+  getFiatSign() {
+    return getFiatSign( this.props.fiatCrypto );
+  }
+
+  getFiatCodeLetter() {
+    return getFiatCodeLetter( this.props.fiatCrypto );
+  }
+
+  generateAxiosUrl() {
+    const nameCrypto = this.props.nameCrypto.toUpperCase();
+    const fiatCodeLetter = getFiatCodeLetter( this.props.fiatCrypto );
+
+    return `https://min-api.cryptocompare.com/data/price?fsym=${nameCrypto}&tsyms=${fiatCodeLetter}`;
+  }
+
+  setIntervalCallback() {
+    const axiosUrl = this.generateAxiosUrl();
+    const fiatCodeLetter = getFiatCodeLetter( this.props.fiatCrypto );
+    axios.get( axiosUrl )
+      .then( response => this.setState({
+        actualPrice: response.data[ fiatCodeLetter ]
+      }));
+  }
+
+  render() {
+    const {
+      dateCrypto, nameCrypto, priceCrypto, amountCrypto
+    } = this.props;
+    const fiatSign = this.getFiatSign();
+    const tradeValue = calculateTradingValue( amountCrypto, priceCrypto );
+
+    return (
+      <Card>
+        <CardHeader tag="h3">
+          { nameCrypto }
+        </CardHeader>
+        <CardBody>
+          <CardTitle>
+            Position Details
+          </CardTitle>
+          <ListGroup>
+            <ListGroupItem color="secondary">
+              Trade date: {dateCrypto}
+            </ListGroupItem>
+            <ListGroupItem color="success">
+              Bought {amountCrypto} {nameCrypto} @ {priceCrypto} = {fiatSign} {tradeValue}
+            </ListGroupItem>
+          </ListGroup>
+        </CardBody>
+        <CardFooter className="text-muted">
+          Trading @ {fiatSign} {this.state.actualPrice}
+        </CardFooter>
+      </Card>
+    );
+  }
+}
--- app/utils/index.js
+++ app/utils/index.js
@@ -1,8 +1,15 @@
-const FIAT_VALUES = {
+const FIAT_SIGN = {
   dollar: '$',
   euro: '€',
   pound: '£'
 };
 
+const FIAT_THREE_CODE_LETTER = {
+  dollar: 'USD',
+  euro: 'EUR',
+  pound: 'GBP'
+};
+
 export const calculateTradingValue = ( amount, price ) => amount * price;
-export const fiatConverter = fiat => FIAT_VALUES[ fiat ];
+export const getFiatSign = fiat => FIAT_SIGN[ fiat ];
+export const getFiatCodeLetter = fiat => FIAT_THREE_CODE_LETTER[ fiat ];
