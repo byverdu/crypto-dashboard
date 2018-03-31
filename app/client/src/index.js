eea9bb01b42794a19a004afa8b17f8623cf92f0b
byverdu <byverdu@gmail.com>
2018-03-31 23:48:38 +0100

    added Redux DevTools Extension config

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 import { cryptoReducers } from './redux';
@@ -10,9 +10,13 @@ import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
 const loggerMiddleware = createLogger();
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+/* eslint-enable */
 const store = createStore(
-  cryptoReducers,
-  applyMiddleware( thunk, loggerMiddleware )
+  cryptoReducers, composeEnhancers(
+    applyMiddleware( thunk, loggerMiddleware )
+  )
 );
 
 ReactDOM.render(
