2298d115ae495e3a8a469bd73cfc61fcf3f50e43
byverdu <byverdu@gmail.com>
2018-04-08 02:28:40 +0100

    solving build... oh my almost

--- app/config/server.js
+++ app/config/server.js
@@ -1,9 +1,12 @@
 import path from 'path';
 
+const isDevEnv = process.env.NODE_ENV === 'development';
+const cryptoPath = isDevEnv ? '../data/test-portfolio.json' : '../data/portfolio.json';
+
 const PORT = 9000;
 const ROOT_PATH = __dirname;
-const CLIENT_PATH = path.join( __dirname, '../client' );
-const CRYPTOFILE_PATH = path.join( __dirname, '../data/portfolio.json' );
+const CLIENT_PATH = path.join( __dirname, '../client/public' );
+const CRYPTOFILE_PATH = path.join( __dirname, `${cryptoPath}` );
 
 export {
   PORT,
