0b5f465aa0a5c4d520a8592cf181ec3edb5e2177
byverdu <byverdu@gmail.com>
2018-04-11 00:14:52 +0100

    modified Info component to accept childre

--- app/client/src/components/Info.js
+++ app/client/src/components/Info.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Alert } from 'reactstrap';
 
-const Info = ({ text, type }) => <Alert color={type}>
+const Info = ({ text, type, children }) => <Alert color={type}>
   {text}
+  {children && children}
 </Alert>;
 
 export default Info;
