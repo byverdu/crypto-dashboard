cbedeb770be345b1109d107712695e82948a462f
byverdu <byverdu@gmail.com>
2018-04-27 00:57:51 +0100

    added images for each crypto

--- app/client/src/components/Tile/TileHeader.js
+++ app/client/src/components/Tile/TileHeader.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { CardHeader, Button } from 'reactstrap';
 
-const TileHedaer = ({ name, onClickRemoveItem, onClickEditItem }) => (
-  <CardHeader tag="h3">
-    {name}
-    <Button onClick={onClickRemoveItem} color="danger">Remove</Button>
-    <Button onClick={onClickEditItem} color="info">Edit</Button>
-  </CardHeader>
-);
+const TileHedaer = ({ name, onClickRemoveItem, onClickEditItem }) => {
+  const imgSrc = `./icon/${name}.svg`;
+  return (
+    <CardHeader tag="h3">
+      {name}
+      <img src={imgSrc} />
+      <Button onClick={onClickRemoveItem} color="danger">Remove</Button>
+      <Button onClick={onClickEditItem} color="info">Edit</Button>
+    </CardHeader>
+  );
+};
 
 export default TileHedaer;
