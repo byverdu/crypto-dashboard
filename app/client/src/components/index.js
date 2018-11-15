8adafab2db25e1b141fb2f7e1ffe36bee94fd0d7
byverdu <byverdu@gmail.com>
2018-11-15 23:42:41 +0000

    deleted old compnents for Forms

--- app/client/src/components/index.js
+++ app/client/src/components/index.js
@@ -1,8 +1,4 @@
-import InputWithError from './InputWithError';
 import Info from './Info';
-import Form from './Form';
-
-import Fieldset from './Fieldset';
 import RadioGroup from './FormElements/RadioGroup';
 import CustomDatePicker from './FormElements/CustomDatepicker';
 import TextFieldGroup from './FormElements/TextFieldGroup';
@@ -10,10 +6,7 @@ import SelectWrapper from './FormElements/SelectWrapper';
 import SelectGroup from './FormElements/SelectGroup';
 
 export {
-  InputWithError,
   Info,
-  Form,
-  Fieldset,
   RadioGroup,
   CustomDatePicker,
   TextFieldGroup,
