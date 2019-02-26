8dd5a62296e141fa7aed4160ae26fae11b1a4bca
byverdu <byverdu@gmail.com>
2019-02-26 00:54:10 +0000

    refactor apiReducer

--- packages/client/src/redux/reducers/apiReducer.js
+++ packages/client/src/redux/reducers/apiReducer.js
@@ -60,6 +60,7 @@ const failedApiCallReducer = handleAction(
     actionsType.ADD_ITEM_TO_API_FAILED,
     actionsType.EDIT_API_ITEM_FAILED,
     actionsType.DELETE_API_ITEM_FAILED,
+    actionsType.FETCH_API_DATA_FAILED,
     actionsType.FETCH_API_DATA_FAILED
   ),
   (
@@ -68,55 +69,17 @@ const failedApiCallReducer = handleAction(
   ) => newStateFailed(
     state, message, status), initialApiState);
 
-
-
-// const addItemsReducer = handleActions({
-//   [ actionsType.ADD_ITEM_TO_API_FAILED ]: (
-//     state,
-//     { payload: { message, status } }
-//   ) => newStateFailed(
-//     state, message, status
-//   )
-// }, initialApiState );
-
 const fetchApiReducer = handleActions({
   [ actionsType.FETCH_API_DATA_SUCCESS ]: (
     state,
     { payload: { data, status } }
   ) => newStateSuccess(
     state, data, status, 'Data fetched from API'
-  ),
-  [ actionsType.FETCH_API_DATA_FAILED ]: (
-    state,
-    { payload: { message, status } }
-  ) => newStateFailed(
-    state, message, status
   )
 }, initialApiState );
 
-// const deleteItemReducer = handleActions({
-//   [ actionsType.DELETE_API_ITEM_FAILED ]: (
-//     state,
-//     { payload: { message, status } }
-//   ) => newStateFailed(
-//     state, message, status
-//   )
-// }, initialApiState );
-
-// const editItemReducer = handleActions({
-//   [ actionsType.EDIT_API_ITEM_FAILED ]: (
-//     state,
-//     { payload: { message, status } }
-//   ) => newStateFailed(
-//     state, message, status
-//   )
-// }, initialApiState );
-
 const apiReducer = mergeReducers(
-  // addItemsReducer,
   fetchApiReducer,
-  // deleteItemReducer,
-  // editItemReducer,
   failedApiCallReducer,
   addReducer,
   deleteReducer,
