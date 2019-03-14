b791ecd0f843bf0bd2258bf10c8bff4398160a4d
byverdu <byverdu@gmail.com>
2019-03-14 00:10:41 +0000

    Updated styles for Summaries components

--- packages/client/src/components/Summary.js
+++ packages/client/src/components/Summary.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, withStyles, Divider } from "@material-ui/core";
+import { Typography, withStyles, Divider, Paper, Grid } from "@material-ui/core";
 import { toLocaleString } from '../clientUtils';
 import red from '@material-ui/core/colors/red';
 import green from '@material-ui/core/colors/green';
@@ -21,10 +21,20 @@ const styles = theme => ({
     background: red[ 300 ],
     color: 'white',
     padding: '10px'
+  },
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  },
+  item: {
+    padding: 10
   }
 });
 
-const checkProfitLost = (amount) => amount > 0 ? 'profit' : 'lost';
+const checkProfitLost = (total, expected) => total > expected ? 'profit' : 'lost';
+const newTotalInvested = (data, totalProfitLost) => `New Total Invested: ${data.length === 0 ? 0 : toLocaleString( totalProfitLost, 4 )}`;
+const newTotalProfitLost = (data, totalProfitLost, totalInvested) => `Total Profit/Lost: ${data.length === 0 ? 0 : toLocaleString( totalProfitLost - totalInvested, 4 )}`
 
 const Summary = ({
   tileSection: {
@@ -34,23 +44,45 @@ const Summary = ({
   api: {data},
   classes
 }) => (
-  <div className={classes.main}>
-    <Typography variant="h3" color="inherit" style={{padding: '10px'}}>
-      Portfolio Resume
-    </Typography>
-    <Divider />
-    <Typography variant="h4" color="inherit" style={{padding: '10px'}}>
-      Total Invested: {toLocaleString(totalInvested)}
-    </Typography>
-    <Divider variant="middle" />
-    <Typography variant="h4" className={classes[checkProfitLost(totalProfitLost)]}>
-      New Total Invested: {data.length === 0 ? 0 : toLocaleString( totalProfitLost, 4 )}
-    </Typography>
-    <Divider variant="middle" />
-    <Typography variant="h4" className={classes[checkProfitLost(totalProfitLost - totalInvested)]}>
-      Total Profit/Lost: {data.length === 0 ? 0 : toLocaleString( totalProfitLost - totalInvested, 4 )}
-    </Typography>
-  </div>
+  <Grid item>
+    <Paper className={classes.root}>
+      <Typography
+        variant="h3"
+        color="inherit"
+        className={classes.item}
+      >
+        Portfolio Resume
+      </Typography>
+
+      <Divider />
+
+      <Typography
+        variant="h4"
+        color="inherit"
+        className={classes.item}
+      >
+        Total Invested: {toLocaleString(totalInvested)}
+      </Typography>
+
+      <Divider variant="middle" />
+
+      <Typography
+        variant="h4"
+        className={classes[checkProfitLost(totalProfitLost, totalInvested)]}
+      >
+        {newTotalInvested(data, totalProfitLost)}
+      </Typography>
+
+      <Divider variant="middle" />
+
+      <Typography
+        variant="h4"
+        className={classes[checkProfitLost((totalProfitLost - totalInvested), 0)]}
+      >
+        {newTotalProfitLost(data, totalProfitLost, totalInvested)}
+      </Typography>
+    </Paper>
+  </Grid>
 );
 
 export default withStyles(styles)(Summary);
--- packages/client/src/components/SummaryCloseTrades.js
+++ packages/client/src/components/SummaryCloseTrades.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import { toLocaleString, formattedDate } from '../clientUtils';
-import green from '@material-ui/core/colors/green';
 import { DATE_FORMAT_TIME } from '../config/client';
-import { Avatar, List, ListItem, ListItemText, ListItemAvatar, Typography, withStyles, Divider } from '@material-ui/core';
+import { Avatar, List, ListItem, ListItemText, ListItemAvatar, Typography, withStyles, Divider, Paper, Grid } from '@material-ui/core';
 
 const styles = theme => ({
-  main: {
-    boxShadow: '0 0 1px',
-    background: 'white',
-    padding: 20,
-    margin: '20px 0',
-    width: 580
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
   },
-  trade: {
-    background: green[300],
-    color: 'white',
-    padding: '10px'
-  }
 });
 
 const getTotalCount = (trades) => {
@@ -37,50 +29,52 @@ const SummaryCloseTrades = ({
   trades,
   classes
 }) => (
-    <div className={classes.main}>
-      <Typography variant="h3" color="inherit" style={{ padding: '10px' }}>
-        Closed Trades
-      </Typography>
-      <Divider />
-      {trades && trades.map(data => (
-        <React.Fragment>
-          <List>
-            <ListItem alignItems="flex-start">
-              <ListItemAvatar>
-                <Avatar
-                    src={`./icon/${data.crypto}.svg`}
-                    alt={`crypto icon for ${data.crypto.toLowerCase()}`}
-                      onError={( e ) => {
-                        e.target.src = './icon/default.svg';
-                      }}
-                  />
-              </ListItemAvatar>
-              <ListItemText>
-                <Typography variant="h6">
-                  Closed Trade for {data.crypto}
-                </Typography>
-              </ListItemText>
-            </ListItem>
-            {data.trades && data.trades.map(trade => (
-                <ListItem key={trade.date} style={{ flexWrap: 'wrap' }}>
-                  <ListItemText primary={formattedDate(trade.date, DATE_FORMAT_TIME)} />
-                  {
-                    trade.closeAmount > 0 &&
-                    <ListItemText
-                      style={{ paddingLeft: 0 }}
-                      primary={textFormatter(trade)}
+    <Grid item>
+      <Paper className={classes.root}>
+        <Typography variant="h3" color="inherit" style={{ padding: '10px' }}>
+          Closed Trades
+        </Typography>
+        <Divider />
+        {trades && trades.map(data => (
+          <React.Fragment>
+            <List>
+              <ListItem alignItems="flex-start">
+                <ListItemAvatar>
+                  <Avatar
+                      src={`./icon/${data.crypto}.svg`}
+                      alt={`crypto icon for ${data.crypto.toLowerCase()}`}
+                        onError={( e ) => {
+                          e.target.src = './icon/default.svg';
+                        }}
                     />
-                  }
-                </ListItem>
-            ))}
-          </List>
-          <Divider />
-        </React.Fragment>
-      ))}
-      <Typography variant="h3" color="inherit" style={{ padding: '10px' }}>
-        Total {getTotalCount(trades)}
-      </Typography>
-    </div>
+                </ListItemAvatar>
+                <ListItemText>
+                  <Typography variant="h6">
+                    Closed Trade for {data.crypto}
+                  </Typography>
+                </ListItemText>
+              </ListItem>
+              {data.trades && data.trades.map(trade => (
+                  <ListItem key={trade.date} style={{ flexWrap: 'wrap' }}>
+                    <ListItemText primary={formattedDate(trade.date, DATE_FORMAT_TIME)} />
+                    {
+                      trade.closeAmount > 0 &&
+                      <ListItemText
+                        style={{ paddingLeft: 0 }}
+                        primary={textFormatter(trade)}
+                      />
+                    }
+                  </ListItem>
+              ))}
+            </List>
+            <Divider />
+          </React.Fragment>
+        ))}
+        <Typography variant="h3" color="inherit" style={{ padding: '10px' }}>
+          Total {getTotalCount(trades)}
+        </Typography>
+      </Paper>
+    </Grid>
   );
 
 export default withStyles(styles)(SummaryCloseTrades);
